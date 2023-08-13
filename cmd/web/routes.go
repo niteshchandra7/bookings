cb7e7d6e1dac1d7801c9afb31295d7a3edeeb7b6
niteshchandra7 <nitesh240295@gmail.com>
2023-08-13 13:19:49 +0530

    secure login with auth middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -45,11 +45,16 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
 	mux.Post("/user/login", handlers.Repo.PostShowLogin)
-	mux.Get("/user/logout", handlers.Repo.ShowLogout)
+	mux.Get("/user/logout", handlers.Repo.Logout)
 
 	mux.Get("/book-room", handlers.Repo.BookRoom)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+	mux.Route("/admin", func(mux chi.Router) {
+		mux.Use(Auth)
+		mux.Get("/dashboard", handlers.Repo.AdminDashboard)
+	})
 	return mux
 }
--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -412,7 +412,7 @@ func (m *Repository) ShowLogin(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
-func (m *Repository) ShowLogout(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 	_ = m.App.Session.Destroy(r.Context())
 	_ = m.App.Session.RenewToken(r.Context())
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
@@ -448,4 +448,8 @@ func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "user_id", id)
 	m.App.Session.Put(r.Context(), "flash", "Logged in sucessfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-dashboard.page.tmpl", &models.TemplateData{})
 }
